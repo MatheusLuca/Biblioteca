adb6bd669423ac4704eaf6bf971c98f90a5efa79
matheus <matheus@DESKTOP-MO8D0IE>
2023-05-06 20:35:10 -0300

    Classe livro

--- Biblioteca/src/classes/Livro.java
+++ Biblioteca/src/classes/Livro.java
@@ -1,7 +1,71 @@
 package classes;
 
-public class Livro {
+public class Livro implements InterfaceBiblioteca {
+
+	private String titulo;
+	private String autor;
+	private int anoPublicacao;
+	private boolean disponivel;
+	
+	public Livro(String titulo, String autor, int anopublicacao) {
+		
+		this.titulo = titulo;
+		this.autor = autor;
+		this.anoPublicacao = anopublicacao;
+		this.disponivel = false;
+	}
+	
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public String getAutor() {
+		return autor;
+	}
+	
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+	
+	
+	@Override
+    public void emprestar() {
+    	if (disponivel) {
+    		disponivel = false;
+    		System.out.println("Livro " + titulo + " emprestado com sucesso!");
+	       
+    	} else {
+	            System.out.println("Livro " + titulo + " indisponível para empréstimo!");
+	        }
+	    }
+	
+	@Override
+		public void devolver() {
+		    if (!disponivel) {
+		        disponivel = true;
+		        System.out.println("Livro " + titulo + " devolvido com sucesso!");
+		    } else {
+		        System.out.println("O livro " + titulo + " não está emprestado!");
+		    }
+		}
+		
+		
 
 	
 	
+	@Override
+	public String exibirInformacao() {
+		
+		return  "Nome do livro " + getTitulo() +
+				" Autor " + getAutor() +
+				" Ano de publicação " + getAnoPublicacao();
+				
+	}
 }
